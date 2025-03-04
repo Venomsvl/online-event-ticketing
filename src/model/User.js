1c3303193fb7f8e6ade1b82f3631dd0cdcd20f28
Venomsvl <Lana.elgayed@student.giu-uni.de>
2025-03-04 14:14:01 +0200

    my final? (Hopefully) update to my part (User Schema)

--- src/model/User.js
+++ src/model/User.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: { type: String, required: flase },
+  email: { type: String, required: false, unique: true },
+  password: { type: String, required: false },
   profilePicture: { type: String }, // URL to the profile picture
   role: { 
     type: String, 
-    enum: ['Standard User', 'Organizer', 'System Admin'], 
+    enum: ['Standard User', 'Organizer' , 'Admin'], 
     default: 'Standard User' 
   },
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User=mongoose.model('User', userSchema);
+module.exports = User;
