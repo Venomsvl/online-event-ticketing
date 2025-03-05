c00d1adf88b044129cf8692dd9f6c43a74436a49
MunaSchool <muna.abdulcadir@student.giu-uni.de>
2025-03-06 00:07:37 +0200

    Updated Booking.js Schema

--- src/model/booking.js
+++ src/model/booking.js
@@ -1,1 +1,28 @@
-//work plz
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema({
+    eventId: {
+     type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event', 
+    required: true
+   },
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true
+},
+     ticketsBooked:{
+         type:Number,
+         required:true
+     },
+     totalPrice:{
+        type:Number,
+        required:true
+    },
+     bookingStatus:{
+        type:String,
+        enum:['Pending','Confirmed','Cancelled'],
+        required:true
+    },
+    createdAt: { type: Date, default: Date.now }
+});
+module.exports=mongoose.model('Booking',booking);
